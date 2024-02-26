2e6bb659ce7cd203fcd59f5416094a590c832ea1
WaqasAamir <waqas.amir@datalatics.com>
2024-02-26 16:20:04 +0500

    Added the contact us page style

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="h-[100px] flex items-center justify-between bg-gray-500 p-4">
+    <div className="mt-10 mb-4 h-[100px] flex items-center justify-between border p-4">
       <div className="font-bold">lamadev</div>
       <div className="text-[12px]">
         Lama creative thoughts agency © All rights reserved.
